763ed0e49a0467b20dafef1af2716b825bd4c8df
Felix Hua <felix.hua@student.tdsb.on.ca>
2026-04-15 10:17:27 -0400

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import { MainLayout } from './layouts/MainLayout';
+import {
+  CareerView,
+  CreditsView,
+  EpisodeView,
+  ErrorView,
+  GenreView,
+  HomeView,
+  ImagesView,
+  MoviesView,
+  MovieView,
+  PersonView,
+  ReviewsView,
+  SearchView,
+  SeasonsView,
+  TelevisionView,
+  TrailersView,
+  TrendingView,
+} from './views';
 
 export const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomeView />} />
+      <Route element={<MainLayout />}>
+        <Route path="/movies">
+          <Route path="category" element={<MoviesView />} />
+          <Route path=":id" element={<MovieView />}>
+            <Route path="credits" element={<CreditsView />} />
+            <Route path="trailers" element={<TrailersView />} />
+            <Route path="reviews" element={<ReviewsView />} />
+          </Route>
+        </Route>
+        <Route path="/tv">
+          <Route path="category" element={<TelevisionView />} />
+          <Route path=":id" element={<MovieView />}>
+            <Route path="seasons" element={<SeasonsView />} />
+            <Route path="season/:season" element={<EpisodeView />} />
+            <Route path="credits" element={<CreditsView />} />
+            <Route path="trailers" element={<TrailersView />} />
+            <Route path="reviews" element={<ReviewsView />} />
+          </Route>
+        </Route>
+        <Route path="/trending" element={<TrendingView />} />
+        <Route path="/genre" element={<GenreView />} />
+        <Route path="/search" element={<SearchView />} />
+        <Route path="/person/id:" element={<PersonView />}>
+          <Route path="career" element={<CareerView />} />
+          <Route path="images" element={<ImagesView />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<ErrorView />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import { Footer, Header } from '../components';
+
+export const MainLayout = () => {
+  return (
+    <div className="min-h-screen bg-indigo-950 text-fuchsia-400">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
